aab5e5bbafb65f3af0ec3dec28fe5a99d1751ae6
Haya A S Sh M Alshamlan <83336@kfh.com>
2024-01-08 15:03:15 +0300

    save

---
+++ leap year/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter the year");
+        int year = myScanner.nextInt();
+
+        boolean isLeapYear;
+
+        isLeapYear = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}
